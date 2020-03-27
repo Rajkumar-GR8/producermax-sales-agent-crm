013b1bbffee5d2fe12039e07affb68c410ce92f2
Rajkumar-GR8 <gobirajkumar1989@gmail.com>
2020-03-27 22:44:28 +0530

    Home page with validation form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { VerificationComponent } from './verification/verification.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ 
+    BrowserModule, 
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HelloComponent },
+    ])
+  ],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent, 
+    HeaderComponent, VerificationComponent 
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
